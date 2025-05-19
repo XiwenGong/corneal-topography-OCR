a78bba52ab91c32f177754d87d86d39e4bc9015d
Xiwen Gong <2503597562@qq.com>
2025-05-19 20:07:42 +0800

    创建 abba.py

---
+++ function_modules/abba.py
@@ -0,0 +1,20 @@
+"""
+@file: abba.py
+@description: 打印 Hello World 的示例脚本
+"""
+
+def hello_world():
+    """
+    打印 Hello World
+    """
+    print("Hello World")
+
+def nihao():
+    """
+    打印 Nihao
+    """
+    print("Nihao")
+
+if __name__ == "__main__":
+    hello_world()
+    nihao()
